77768d6f290bdd9d18f76ea5b428302c82d5224e
King-of-Sea <72517825+King-of-Sea@users.noreply.github.com>
2020-11-07 21:13:59 +0700

    Add files via upload

---
+++ a3.js
@@ -0,0 +1,88 @@
+// // let n = Number(prompt("Nhap n vao day!"));
+// // let a = 0;
+// // for(let i=1; i<=n; i=i+1) {
+// //     a=a+i;
+// // }
+// // console.log(`A = ${a}`);
+
+// // // a=0
+// // // lần lặp đầu tiên, i=1 => a=0 + 1=1
+// // // lần lặp thứ 2, i=2 => a=1 +2 =3
+// // // lần lặp thứ 3
+
+// // let x = Number(prompt("Nhập số khác vào:"));
+// // let b = 0
+// // for(let y=1; y<=x; y++) {
+// //     b = b + y*y ;
+// // }
+// // console.log(`B = ${b}`);
+
+// // let z = Number(prompt("Nhập số khác nữa vào:"));
+// // let c = 1
+// // for(let i=2; i<=z; i++) {
+// //     c = c + 1/(i*(i+1))  ;
+// // }
+// // console.log(`C=${c}`);
+
+// let n = prompt("Nhập số bất kì");
+//     if (n<0) {
+//         console.log("invalid input");
+//     } 
+//     else if (n>=0) {
+//         let a = 1
+//         for(let i=1; i<=n; i++) {
+//             a = a * i ;
+//         }
+//         console.log(`A=${a}`)
+//     }
+
+
+// let name = prompt("tell me your name")
+// let age = prompt("tell me your age")
+//     if (age < 18) {
+//         console.log( "chưa đủ tuổi vào trang web này")
+//     }
+//     else if (age >=18) {
+//         let userInput= prompt(`${name} có muốn vào trang web này không?`)
+
+//         if (userInput == "yes") {
+//             console.log(`${name} đã vào trang web thành công`)
+
+//         }
+//         else if (userInput == "no") {
+//             console.log(`${name} out from this webside`)
+//         }
+//     }
+
+// let username = prompt("username");
+// let password = prompt("password");
+// let username2 = prompt("type your username again") ;
+// let password2 = prompt("type password again")
+    // if (username === username2 && password === password2 ) {
+    //        console.log("success Input")
+    //    }
+    //    else if (username2 !== username && password2 !== password) {
+    //                console.log(`${username}failed Input`)
+    //    } 
+
+
+    // let name = prompt("please write your name");
+    // let month = prompt("please enter your birthday");
+    //     if (month < 4 ) {
+    //         console.log(`${name}, bạn sinh vào mùa xuân `)
+    //     }
+
+    //     else if (month < 7) {
+    //         console.log(`${name}, bạn sinh vào mùa hạ `)
+    //     }
+
+    //     else if (month < 10) {
+    //         console.log(`${name}, bạn sinh vào mùa thu `)
+    //     }
+
+    //     else if (month < 13) {
+    //         console.log(`${name}, bạn sinh vào mùa đông `)
+    //     }
+    
+
+    
